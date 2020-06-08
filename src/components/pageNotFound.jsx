dae3c156335b9b27d8327af19eea55b9007945f6
tm1287 <tejas.maraliga@gmail.com>
2020-06-08 17:27:56 -0400

    Add proper css to 404 image

--- src/components/pageNotFound.jsx
+++ src/components/pageNotFound.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageNotFoundImage from '../PageNotFoundImage.png';
+import "../App.css";
+
 
 class PageNotFound extends React.Component{
+  
     render(){
-        return <div>
-            <img src={PageNotFoundImage} width="740px"/>
-            <p style={{textAlign:"center"}}>
-              <Link to="/">Go to Home </Link>
-            </p>
-          </div>;
+        return<div>
+          <img className="image_404" src={PageNotFoundImage}/>
+          <p style={{textAlign:"center"}}>
+            <Link to="/">Go to Home </Link>
+          </p>
+        </div>;
     }
 }export default PageNotFound;
