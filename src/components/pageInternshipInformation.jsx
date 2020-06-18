d470117865818a43df66be9e726aafbd1b73b7d1
tm1287 <tejas.maraliga@gmail.com>
2020-06-17 21:55:18 -0400

    Remove test button on first page.

--- src/components/pageInternshipInformation.jsx
+++ src/components/pageInternshipInformation.jsx
@@ -575,14 +575,6 @@ class PageInternshipInformation extends Component {
               <Button className="next-button" type="primary" htmlType="submit">
                 Save and Continue
               </Button>
-              <Button
-                className="next-button"
-                type="default"
-                htmlType="button"
-                onClick={this.testFunc}
-              >
-                Test
-              </Button>
             </Form.Item>
           </Form>
         </Spin>
