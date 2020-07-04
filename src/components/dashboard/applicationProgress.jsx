11de82d2ca67d531d6d5e437b85d1c1c82094dec
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2020-07-04 16:01:44 -0400

    Progress bar completion mode

--- src/components/dashboard/applicationProgress.jsx
+++ src/components/dashboard/applicationProgress.jsx
@@ -4,14 +4,25 @@ import "../../App.css";
 import "./dashboard.css";
 import "antd/dist/antd.css";
 import { Progress } from "antd";
+import { CheckCircleTwoTone } from "@ant-design/icons";
 
+/*
+
+Container to hold all the progress bars
+
+*/
 const ModuleContainer = styled.div`
   background: white;
   border-radius: 10px;
-  height: 440px;
   padding: 3%;
 `;
 
+/*
+
+Header for name of the progress module
+(e.g. Internship Information, Personal, Essays, References)
+
+*/
 const ProgressHeader = styled.h2`
   margin-bottom: 7px;
   display: inline;
@@ -25,6 +36,11 @@ const ProgressHeader = styled.h2`
   color: black;
 `;
 
+/*
+
+Header for the percent complete
+
+*/
 const PercentHeader = styled.h2`
   display: inline;
   float: right;
@@ -33,6 +49,11 @@ const PercentHeader = styled.h2`
   font-size: 18px;
 `;
 
+/*
+
+Link at the bottom of the progress bar to open a custom checklist
+
+*/
 const ViewChecklist = styled.p`
   margin-top: 4px;
   margin-bottom: 15px;
@@ -72,11 +93,20 @@ class ApplicationProgress extends Component {
           {percentComplete.map(section => (
             <React.Fragment>
               <ProgressHeader>{section[0]}</ProgressHeader>
-              <PercentHeader>{section[1]}%</PercentHeader>
+              <PercentHeader>
+                {section[1] < 100 ? (
+                  section[1] + "%"
+                ) : (
+                  <CheckCircleTwoTone
+                    style={{ fontSize: "24px" }}
+                    twoToneColor="#52c41a"
+                  />
+                )}
+              </PercentHeader>
               <Progress
                 percent={section[1]}
                 trailColor={section[2]}
-                strokeColor={section[3]}
+                strokeColor={section[1] < 100 ? section[3] : "#52c41a"}
                 strokeWidth="15px"
                 status="active"
                 showInfo={false}
