2c94dc41d2728164fa2ca951c312fdf7048091b3
Brandon Lu <brandonbl2021@gmail.com>
2020-07-10 13:25:48 -0400

    filter industries

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -62,14 +62,7 @@ class Dashboard extends Component {
     );
   }
 
-  componentDidMount() {
-    fetch('https://api.indeed.com/ads/apisearch?publisher=123412341234123&q=java+developer&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2')
-      .then(response =>
-        response.json().then(data => {
-          this.setState({ info: data });
-        })
-      )
-  }
+
 
 
 
