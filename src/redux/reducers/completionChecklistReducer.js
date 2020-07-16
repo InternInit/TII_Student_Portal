cd477428644fc3c96823f02caf7b6c7562353750
tm1287 <tejas.maraliga@gmail.com>
2020-07-16 00:33:21 -0400

    Add redux reducer for completion checklist

---
+++ src/redux/reducers/completionChecklistReducer.js
@@ -0,0 +1,11 @@
+const completionChecklistReducer = (state = [[],[],[],[]], action) => {
+  switch (action.type) {
+    case 'UPDATE_COMPLETION_CHECKLIST':
+      state[action.page] = action.completionChecklist
+      return state
+    default:
+      return state
+  }
+}
+
+export default completionStateReducer
