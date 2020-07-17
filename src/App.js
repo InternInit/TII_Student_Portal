1a67d840fb110458cc01661b3e5dfa6e6ded8f43
tm1287 <tejas.maraliga@gmail.com>
2020-07-16 22:42:19 -0400

    Initialize checklist frame even if empty

--- src/App.js
+++ src/App.js
@@ -283,6 +283,9 @@ class App extends Component {
 
   getCachedCompletionState = async () => {
     let token = await this.getJwt();
+    //TODO: Implement a better way of defaulting
+    let defaultChecklist = [[{'key': 'First Name', 'completed': false}, {'key': 'Last Name', 'completed': false}, {'key': 'Phone Number', 'completed': false}, {'key': 'Email', 'completed': false}, {'key': 'Address', 'completed': false}, {'key': 'City', 'completed': false}, {'key': 'State', 'completed': false}, {'key': 'Zip Code', 'completed': false}, {'key': 'Year Of Graduation', 'completed': false}, {'key': 'Interested Industries', 'completed': false}, {'key': 'Unweighted GPA', 'completed': false}, {'key': 'Relevant Courses', 'completed': false}, {'key': 'Extracurriculars', 'completed': false}, {'key': 'Willing Work Days', 'completed': false}, {'key': 'Willing Work Times', 'completed': false}, {'key': 'Starting/Ending Dates', 'completed': false}, {'key': 'Paid/Unpaid Preference', 'completed': false}, {'key': 'Resume', 'completed': false}], [{'key': 'Gender', 'completed': false}, {'key': 'Age', 'completed': false}, {'key': 'Education', 'completed': false}], [{'key': 'Why This Industry Essay', 'completed': false}, {'key': 'Leadership Roles Essay', 'completed': false}, {'key': 'Extra Essay', 'completed': false}, {'key': 'Cover Letter', 'completed': false}], [{'key': 'Reference', 'completed': false}]]
+
     fetch("/api/get_user_data", {
       method: "POST",
       headers: {
@@ -300,7 +303,7 @@ class App extends Component {
           this.props.batchUpdateCompletionChecklist(recvCompletionChecklist)
         } else {
           this.props.batchUpdateCompletionState([0,0,0,0]);
-          this.props.batchUpdateCompletionChecklist([[],[],[],[]])
+          this.props.batchUpdateCompletionChecklist(defaultChecklist)
         }
       });
   };
