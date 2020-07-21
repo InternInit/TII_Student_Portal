5632d1eaff515baf5fd006f85c91fb7d93180786
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2020-07-21 16:28:37 -0400

    Added array to applyskills page

--- src/components/dashboard/apply-skills/applySkills.jsx
+++ src/components/dashboard/apply-skills/applySkills.jsx
@@ -11,6 +11,103 @@ import { Card, Row, Col } from "antd";
 import { Redirect, Link } from "react-router-dom";
 import { withRouter } from "react-router";
 
+const resumeSkills = [
+  [
+    "Choosing_Resume",
+    "How to Choose Which Style Resume to Use",
+    "5 minutes",
+    "Find out which resume style works best for you."
+  ],
+  [
+    "What_To_Include_On_Resume",
+    "What You Should Include on Your Resume",
+    "25 minutes",
+    "How to decide what to include on your resume."
+  ],
+  [
+    "Writing_Reverse_Chronological",
+    "How to Write a Reverse Chronological Resume",
+    "15 minutes",
+    "If you’ve had prior job experiences, read on to learn how to create a reverse chronological resume."
+  ],
+  [
+    "Writing_Functional_Resume",
+    "How to Write a Functional Resume",
+    "15 minutes",
+    "If you don’t have any prior work experience, don’t fret. Read on to learn how to create a functional resume."
+  ],
+  [
+    "Writing_Combination_Resume",
+    "How to Write a Combination Resume",
+    "15 minutes",
+    "If you want to show your skills AND work experience, a combination resume may be for you. Learn how to create a combination resume."
+  ],
+  [
+    "Reverse_Chron_Resume_Example",
+    "An Example of a Reverse Chronological Resume",
+    "15 minutes",
+    "Take a look at what a reverse chronological resume could look like."
+  ],
+  [
+    "Funct_Resume_Example",
+    "An Example of a Functional Resume",
+    "15 minutes",
+    "Take a look at what a functional resume could look like."
+  ],
+  [
+    "Comb_Resume_Example",
+    "An Example of a Combination Resume",
+    "15 minutes",
+    "Take a look at what a combination resume could look like."
+  ]
+];
+const interviewSkills = [
+  [
+    "What_to_Say_in_Interview",
+    "What you Should Say in an Interview",
+    "10 minutes",
+    "Prepare yourself for a job interview by reviewing what you should (and shouldn’t) say."
+  ],
+  [
+    "How_to_act_Interview",
+    "How to Act in an Interview",
+    "10 minutes",
+    "There are many Do’s and Do NOT’s in job interviews. Learn more about the various facets of the interview process so you can land your internship."
+  ],
+  [
+    "Before_After_Interview",
+    "What to do Before and After an Interview",
+    "7 minutes",
+    "Give yourself an edge in the interview process by preparing before it begins and making yourself more memorable afterwards."
+  ],
+  [
+    "Mock_Interview_Questions",
+    "Potential Interview Questions",
+    "10 minutes",
+    "12 questions that you might be asked during an interview."
+  ]
+];
+const writtenSkills = [
+  [
+    "Letter_of_Rec",
+    "A Guide to Letters of Recommendations",
+    "20 minutes",
+    "What is a letter of recommendation, and how do you get one? These questions (among many others) will all be answered in this quick guide."
+  ],
+  [
+    "Cover_Letter",
+    "How to Write a Cover Letter",
+    "10 minutes",
+    "A quick tutorial explaining cover letters and how to write them."
+  ],
+  [
+    "Extra_Info",
+    "Additional Resources",
+    "2 minutes",
+    "Just in case we missed something, here are some outside resources we recommend to answer your questions."
+  ]
+];
+
 class ApplySkills extends Component {
   render() {
     return (
