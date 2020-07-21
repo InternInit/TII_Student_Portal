6c5f6d07a2015a4a0802d56ae817b80316ee94b0
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2020-07-21 17:04:07 -0400

    Rendered all the applyskills cards

--- src/components/dashboard/apply-skills/applySkills.jsx
+++ src/components/dashboard/apply-skills/applySkills.jsx
@@ -4,7 +4,24 @@ import styled from "styled-components";
 import "../../../App.css";
 import "../dashboard.css";
 import GettingStarted from "./GettingStarted.jpg";
+import Choosing_Resume from "./tiiArrayPictures/Choosing_Resume.jpg";
+import What_To_Include_On_Resume from "./tiiArrayPictures/What_To_Include_On_Resume.jpg";
+import Writing_Reverse_Chronological from "./tiiArrayPictures/Writing_Reverse_Chronological.jpg";
+import Writing_Functional_Resume from "./tiiArrayPictures/Writing_Functional_Resume.jpg";
+import Writing_Combination_Resume from "./tiiArrayPictures/Writing_Combination_Resume.jpg";
+import Reverse_Chron_Resume_Example from "./tiiArrayPictures/Reverse_Chron_Resume_Example.jpg";
+import Funct_Resume_Example from "./tiiArrayPictures/Funct_Resume_Example.jpg";
+import Comb_Resume_Example from "./tiiArrayPictures/Comb_Resume_Example.jpg";
+import What_to_Say_in_Interview from "./tiiArrayPictures/What_to_Say_in_Interview.jpg";
+import How_to_act_Interview from "./tiiArrayPictures/How_to_act_Interview.jpg";
+import Before_After_Interview from "./tiiArrayPictures/Before_After_Interview.jpg";
+import Mock_Interview_Questions from "./tiiArrayPictures/Mock_Interview_Questions.jpg";
+import Letter_of_Rec from "./tiiArrayPictures/Letter_of_Rec.jpg";
+import Cover_Letter from "./tiiArrayPictures/Cover_Letter.jpg";
+import Extra_Info from "./tiiArrayPictures/Extra_Info.jpg";
+
 import BigCard from "./bigCard";
+import LittleCard from "./littleCard";
 
 import { Card, Row, Col } from "antd";
 
@@ -13,75 +30,75 @@ import { withRouter } from "react-router";
 
 const resumeSkills = [
   [
-    "Choosing_Resume",
+    Choosing_Resume,
     "How to Choose Which Style Resume to Use",
     "5 minutes",
     "Find out which resume style works best for you."
   ],
   [
-    "What_To_Include_On_Resume",
+    What_To_Include_On_Resume,
     "What You Should Include on Your Resume",
     "25 minutes",
     "How to decide what to include on your resume."
   ],
   [
-    "Writing_Reverse_Chronological",
-    "How to Write a Reverse Chronological Resume",
+    Writing_Reverse_Chronological,
+    "Writing a Reverse Chronological Resume",
     "15 minutes",
     "If you’ve had prior job experiences, read on to learn how to create a reverse chronological resume."
   ],
   [
-    "Writing_Functional_Resume",
-    "How to Write a Functional Resume",
+    Writing_Functional_Resume,
+    "Writing a Functional Resume",
     "15 minutes",
     "If you don’t have any prior work experience, don’t fret. Read on to learn how to create a functional resume."
   ],
   [
-    "Writing_Combination_Resume",
-    "How to Write a Combination Resume",
+    Writing_Combination_Resume,
+    "Writing a Combination Resume",
     "15 minutes",
     "If you want to show your skills AND work experience, a combination resume may be for you. Learn how to create a combination resume."
   ],
   [
-    "Reverse_Chron_Resume_Example",
-    "An Example of a Reverse Chronological Resume",
+    Reverse_Chron_Resume_Example,
+    "Reverse Chronological Resume Example",
     "15 minutes",
     "Take a look at what a reverse chronological resume could look like."
   ],
   [
-    "Funct_Resume_Example",
-    "An Example of a Functional Resume",
+    Funct_Resume_Example,
+    "Functional Resume Example",
     "15 minutes",
     "Take a look at what a functional resume could look like."
   ],
   [
-    "Comb_Resume_Example",
-    "An Example of a Combination Resume",
+    Comb_Resume_Example,
+    "Combination Resume Example",
     "15 minutes",
     "Take a look at what a combination resume could look like."
   ]
 ];
 const interviewSkills = [
   [
-    "What_to_Say_in_Interview",
+    What_to_Say_in_Interview,
     "What you Should Say in an Interview",
     "10 minutes",
     "Prepare yourself for a job interview by reviewing what you should (and shouldn’t) say."
   ],
   [
-    "How_to_act_Interview",
+    How_to_act_Interview,
     "How to Act in an Interview",
     "10 minutes",
     "There are many Do’s and Do NOT’s in job interviews. Learn more about the various facets of the interview process so you can land your internship."
   ],
   [
-    "Before_After_Interview",
+    Before_After_Interview,
     "What to do Before and After an Interview",
     "7 minutes",
     "Give yourself an edge in the interview process by preparing before it begins and making yourself more memorable afterwards."
   ],
   [
-    "Mock_Interview_Questions",
+    Mock_Interview_Questions,
     "Potential Interview Questions",
     "10 minutes",
     "12 questions that you might be asked during an interview."
@@ -89,19 +106,19 @@ const interviewSkills = [
 ];
 const writtenSkills = [
   [
-    "Letter_of_Rec",
+    Letter_of_Rec,
     "A Guide to Letters of Recommendations",
     "20 minutes",
     "What is a letter of recommendation, and how do you get one? These questions (among many others) will all be answered in this quick guide."
   ],
   [
-    "Cover_Letter",
+    Cover_Letter,
     "How to Write a Cover Letter",
     "10 minutes",
     "A quick tutorial explaining cover letters and how to write them."
   ],
   [
-    "Extra_Info",
+    Extra_Info,
     "Additional Resources",
     "2 minutes",
     "Just in case we missed something, here are some outside resources we recommend to answer your questions."
@@ -122,222 +139,41 @@ class ApplySkills extends Component {
         internships and how The Internship Initiative can help you get
         one."
         />
-        <Link to="#">
-          <Card
-            style={{ width: "100%", borderRadius: "15px", border: "none" }}
-            cover={
-              <img
-                id="getting-started-card"
-                className="apply-skills-big-card-cover"
-                alt="Document and folder"
-                src={GettingStarted}
-              />
-            }
-            hoverable
-          >
-            <Card.Meta></Card.Meta>
-            <h1 className="apply-skills-big-card-title">
-              Why do I need an internship?
-            </h1>
-            <p className="apply-skills-big-card-content">
-              Curious about why you would need an internship? Check out our
-              comprehensive overview about the benefits of high school
-              internships and how The Internship Initiative can help you get
-              one.
-            </p>
-          </Card>
-        </Link>
         <h1 className="module-name">Build your resumé</h1>
-        <Row>
-          <Col></Col>
+        <Row gutter={[32, 32]}>
+          {resumeSkills.map(module => (
+            <LittleCard
+              link="#"
+              cover={module[0]}
+              title={module[1]}
+              readTime={module[2]}
+              description={module[3]}
+            />
+          ))}
         </Row>
-        <Row gutter={[32, 16]}>
-          <Col span={12}>
-            <Link to="#">
-              <Card
-                className="apply-skills-little-card"
-                style={{
-                  width: "100%",
-                  borderRadius: "15px",
-                  border: "none",
-                  display: "inline-block"
-                }}
-                cover={
-                  <img
-                    className="apply-skills-little-card-cover"
-                    alt="Document and folder"
-                    src={GettingStarted}
-                  />
-                }
-                hoverable
-              >
-                <h1 className="apply-skills-little-card-title">
-                  Why do I need an internship?
-                </h1>
-                <p className="apply-skills-little-card-content">
-                  Curious about why you would need an internship? Check out our
-                  comprehensive overview about the benefits of high school
-                  internships and how The Internship Initiative can help you get
-                  one.
-                </p>
-              </Card>
-            </Link>
-          </Col>
-          <Col span={12}>
-            <Link to="#">
-              <Card
-                style={{
-                  width: "100%",
-                  borderRadius: "15px",
-                  border: "none",
-                  display: "inline-block"
-                }}
-                cover={
-                  <img
-                    className="apply-skills-little-card-cover"
-                    alt="Document and folder"
-                    src={GettingStarted}
-                  />
-                }
-                hoverable
-              >
-                <Card.Meta></Card.Meta>
-                <h1 className="apply-skills-little-card-title">
-                  Why do I need an internship?
-                </h1>
-                <p className="apply-skills-little-card-content">
-                  Curious about why you would need an internship? Check out our
-                  comprehensive overview about the benefits of high school
-                  internships and how The Internship Initiative can help you get
-                  one.
-                </p>
-              </Card>
-            </Link>
-          </Col>
-          <Col span={12}>
-            <Link to="#">
-              <Card
-                className="apply-skills-little-card"
-                style={{
-                  width: "100%",
-                  borderRadius: "15px",
-                  border: "none",
-                  display: "inline-block"
-                }}
-                cover={
-                  <img
-                    className="apply-skills-little-card-cover"
-                    alt="Document and folder"
-                    src={GettingStarted}
-                  />
-                }
-                hoverable
-              >
-                <h1 className="apply-skills-little-card-title">
-                  Why do I need an internship?
-                </h1>
-                <p className="apply-skills-little-card-content">
-                  Curious about why you would need an internship? Check out our
-                  comprehensive overview about the benefits of high school
-                  internships and how The Internship Initiative can help you get
-                  one.
-                </p>
-              </Card>
-            </Link>
-          </Col>
-          <Col span={12}>
-            <Link to="#">
-              <Card
-                style={{
-                  width: "100%",
-                  borderRadius: "15px",
-                  border: "none",
-                  display: "inline-block"
-                }}
-                cover={
-                  <img
-                    className="apply-skills-little-card-cover"
-                    alt="Document and folder"
-                    src={GettingStarted}
-                  />
-                }
-                hoverable
-              >
-                <Card.Meta></Card.Meta>
-                <h1 className="apply-skills-little-card-title">
-                  Why do I need an internship?
-                </h1>
-                <p className="apply-skills-little-card-content">
-                  Curious about why you would need an internship? Check out our
-                  comprehensive overview about the benefits of high school
-                  internships and how The Internship Initiative can help you get
-                  one.
-                </p>
-              </Card>
-            </Link>
-          </Col>
-          <Col span={12}>
-            <Link to="#">
-              <Card
-                className="apply-skills-little-card"
-                style={{
-                  width: "100%",
-                  borderRadius: "15px",
-                  border: "none",
-                  display: "inline-block"
-                }}
-                cover={
-                  <img
-                    className="apply-skills-little-card-cover"
-                    alt="Document and folder"
-                    src={GettingStarted}
-                  />
-                }
-                hoverable
-              >
-                <h1 className="apply-skills-little-card-title">
-                  Why do I need an internship?
-                </h1>
-                <p className="apply-skills-little-card-content">
-                  Curious about why you would need an internship? Check out our
-                  comprehensive overview about the benefits of high school
-                  internships and how The Internship Initiative can help you get
-                  one.
-                </p>
-              </Card>
-            </Link>
-          </Col>
-          <Col span={12}>
-            <Link to="#">
-              <Card
-                style={{
-                  width: "100%",
-                  borderRadius: "15px",
-                  border: "none",
-                  display: "inline-block"
-                }}
-                cover={
-                  <img
-                    className="apply-skills-little-card-cover"
-                    alt="Document and folder"
-                    src={GettingStarted}
-                  />
-                }
-                hoverable
-              >
-                <Card.Meta></Card.Meta>
-                <h1 className="apply-skills-little-card-title">
-                  Why do I need an internship?
-                </h1>
-                <p className="apply-skills-little-card-content">
-                  Curious about why you would need an internship? Check out our
-                  comprehensive overview about the benefits of high school
-                  internships and how The Internship Initiative can help you get
-                  one.
-                </p>
-              </Card>
-            </Link>
-          </Col>
+        <h1 className="module-name">Ace your Interview</h1>
+        <Row gutter={[32, 32]}>
+          {interviewSkills.map(module => (
+            <LittleCard
+              link="#"
+              cover={module[0]}
+              title={module[1]}
+              readTime={module[2]}
+              description={module[3]}
+            />
+          ))}
+        </Row>
+        <h1 className="module-name">Master the Written Work</h1>
+        <Row gutter={[32, 32]}>
+          {writtenSkills.map(module => (
+            <LittleCard
+              link="#"
+              cover={module[0]}
+              title={module[1]}
+              readTime={module[2]}
+              description={module[3]}
+            />
+          ))}
         </Row>
       </div>
     );
