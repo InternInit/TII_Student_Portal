e55308e2215cf41a4fad616032185d2e54478371
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2020-10-24 17:33:09 -0400

    Responsive About Section stacking

--- src/components/dashboard/CompanyInformation.js
+++ src/components/dashboard/CompanyInformation.js
@@ -260,52 +260,7 @@ class CompanyInformation extends React.Component {
                   </h1>
                 </AntRow>
 
-                <AntRow gutter={[32, 16]}>
-                  <AntCol span={12}>
-                    <div className="company-info-sub-card">
-                      <h1 className="sub-card-heading twentyEightFont">
-                        Internship Facts
-                      </h1>
-                      <h2 className="sub-card-sub-heading sixteenFont">
-                        Industry
-                      </h2>
-                      <p className="sixteenFont">{info.industry}</p>
-                      <h2 className="sub-card-sub-heading sixteenFont">
-                        Work Time
-                      </h2>
-                      <p className="sixteenFont">
-                        {info.starttime} - {info.endtime}
-                      </p>
-                      <h2 className="sub-card-sub-heading sixteenFont">
-                        Additional Information
-                      </h2>
-                      <p className="sixteenFont">
-                        - AP CSA - AP CSP - Must be 18+
-                      </p>
-                    </div>
-                  </AntCol>
-                  <AntCol span={12}>
-                    <div className="company-info-sub-card">
-                      <h1 className="sub-card-heading twentyEightFont">
-                        Contact
-                      </h1>
-                      <h2 className="sub-card-sub-heading sixteenFont">
-                        Website
-                      </h2>
-                      <p className="sixteenFont">
-                        <a href={info.website}>{info.website}</a>
-                      </p>
-                      <h2 className="sub-card-sub-heading sixteenFont">
-                        Email
-                      </h2>
-                      <p className="sixteenFont">{info.email}</p>
-                      <h2 className="sub-card-sub-heading sixteenFont">
-                        Phone Number
-                      </h2>
-                      <p className="sixteenFont">{info.phonenumber}</p>
-                    </div>
-                  </AntCol>
-                </AntRow>
+                <AboutSection info={info} />
 
                 {/**
                  *
@@ -372,6 +327,46 @@ const RenderListings = (props) => {
         ))}
       </AntRow>
     </div>
+  );
+};
+
+const AboutSection = ({ info }) => {
+
+  const screens = useBreakpoint();
+  const isDesktop =
+    Object.entries(screens).filter((screen) => !!screen[1]).length > 2;
+
+  return (
+    <AntRow gutter={[32, 16]}>
+      <AntCol span={isDesktop ? 12 : 24}>
+        <div className="company-info-sub-card">
+          <h1 className="sub-card-heading twentyEightFont">Internship Facts</h1>
+          <h2 className="sub-card-sub-heading sixteenFont">Industry</h2>
+          <p className="sixteenFont">{info.industry}</p>
+          <h2 className="sub-card-sub-heading sixteenFont">Work Time</h2>
+          <p className="sixteenFont">
+            {info.starttime} - {info.endtime}
+          </p>
+          <h2 className="sub-card-sub-heading sixteenFont">
+            Additional Information
+          </h2>
+          <p className="sixteenFont">- AP CSA - AP CSP - Must be 18+</p>
+        </div>
+      </AntCol>
+      <AntCol span={isDesktop ? 12 : 24}>
+        <div className="company-info-sub-card">
+          <h1 className="sub-card-heading twentyEightFont">Contact</h1>
+          <h2 className="sub-card-sub-heading sixteenFont">Website</h2>
+          <p className="sixteenFont">
+            <a href={info.website}>{info.website}</a>
+          </p>
+          <h2 className="sub-card-sub-heading sixteenFont">Email</h2>
+          <p className="sixteenFont">{info.email}</p>
+          <h2 className="sub-card-sub-heading sixteenFont">Phone Number</h2>
+          <p className="sixteenFont">{info.phonenumber}</p>
+        </div>
+      </AntCol>
+    </AntRow>
   );
 };
 
